c2346052034481c2083e93476e12a634957e2a46
HadesDerGrosse <markus_hadinger@yahoo.de>
2016-12-17 14:59:41 +0100

    Hades Stuff

---
+++ gamejam2016/Assets/Logic/Graphic/animatedTexture.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animatedTexture : MonoBehaviour {
+
+
+    public int fps = 12;
+    public float tileSizeX = 0.25f;
+    public float tileSizeY = 0.25f;
+    public float offsetX = 0.25f;
+    public float offsetY = 0.25f;
+
+
+    private float currentX = 0;
+    private float currentY = 0;
+    private float nextFrame = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Time.time > nextFrame)
+        {
+            currentX += tileSizeX;
+            currentY += ((int)currentX / 1) * tileSizeY;
+            currentX %= 1;
+            currentY %= 1;
+
+            currentX = Mathf.Max(offsetX, currentX);
+            currentY = Mathf.Max(offsetY, currentY);
+
+            nextFrame += 1 / fps;
+
+            Debug.Log(currentX + " " + currentY);
+
+            GetComponent<SkinnedMeshRenderer>().material.mainTextureOffset = new Vector2(currentX, currentY);
+        }
+
+
+        
+	}
+}
---
+++ gamejam2016/Assets/Logic/Graphic/sinusmovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sinusmovement : MonoBehaviour {
+
+    public Vector3[] directions;
+    public float[] frequences;
+
+    private Vector3 startPos;
+    private float offset;
+
+
+	// Use this for initialization
+	void Start () {
+        startPos = this.transform.position;
+        offset = UnityEngine.Random.Range(0, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 movement = Vector3.zero;
+
+        for(int i=0; i<directions.Length; i++)
+        {
+            movement += Mathf.Sin(Time.time * frequences[i]+offset) * directions[i];
+        }
+
+        this.transform.position = startPos + movement;
+
+	}
+}
