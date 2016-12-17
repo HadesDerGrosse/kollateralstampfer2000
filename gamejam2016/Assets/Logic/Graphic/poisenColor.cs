713f61cc9ffdb807cd904021dafdbf8253af9bb0
HadesDerGrosse <markus_hadinger@yahoo.de>
2016-12-17 16:44:19 +0100

    Hades stuff

---
+++ gamejam2016/Assets/Logic/Graphic/poisenColor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class poisenColor : MonoBehaviour {
+
+    public Color infectedColor;
+    public Color deadColor;
+
+    public SpriteRenderer head;
+
+    private Player player;
+    
+	
+    void Awake()
+    {
+        player = GetComponent<Player>();
+    }
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!player.infected)
+            head.material.color = Color.white;
+
+        else
+        {
+           // head.material.color = Color.Lerp();
+        }
+
+	}
+}
