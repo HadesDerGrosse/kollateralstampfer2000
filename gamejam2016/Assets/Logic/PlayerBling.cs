34ac38b7019d07b9eaf21f1d00338467d35fb556
Viele <chris.lend@gmx.at>
2016-12-18 12:30:28 +0100

    bling waves

---
+++ gamejam2016/Assets/Logic/PlayerBling.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBling : MonoBehaviour {
+
+    public GameObject blingEffect;
+    public float cooldown = 0.5f;
+
+    private Player player;
+    private float triggerTime = 0;
+
+    void Awake()
+    {
+        player = GetComponent<Player>();
+    }
+	
+	void Update () {
+        if (Input.GetButton("Fire_" + player.pNum) && Time.time - triggerTime > cooldown)
+        {
+            Instantiate(blingEffect, transform.position, Quaternion.identity);
+            triggerTime = Time.time;
+        }
+
+	}
+}
