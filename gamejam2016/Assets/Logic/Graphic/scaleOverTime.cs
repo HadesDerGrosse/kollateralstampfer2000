2878e303e9cb5e810530d8aebaeb7e5fb73bff0e
HadesDerGrosse <markus_hadinger@yahoo.de>
2016-12-18 10:46:52 +0100

    wave and jiggle

---
+++ gamejam2016/Assets/Logic/Graphic/scaleOverTime.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scaleOverTime : MonoBehaviour {
+
+    public AnimationCurve sizeOverTime;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.localScale = Vector3.one * sizeOverTime.Evaluate(Time.time);
+
+    }
+}
